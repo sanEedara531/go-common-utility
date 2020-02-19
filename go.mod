c59501000b0fa96f476067219817ab04d8c40080
Eedara Satya Sandeep <eedarasatya.s@delhivery.com>
2020-02-19 15:10:13 +0530

    Modifying go.mod file

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
-module github.com/sanEedara531/go-common-utility
+module common
 
-go 1.13.5
+go 1.13
+
+require (
+	github.com/aws/aws-sdk-go v1.28.12
+	github.com/google/uuid v1.1.1
+	go.uber.org/zap v1.13.0
+)
--- logger.go
+++ logger.go
@@ -16,12 +16,6 @@ import (
 	"time"
 )
 
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
 func Logger(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
